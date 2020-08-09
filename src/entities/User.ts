d51f75ddcbd6cf707640c2e62182acfff2eb2a02
jinwoong-bang <jinwoong-bang@jinwoong-bangui-MacBookPro.local>
2020-08-09 18:26:52 +0900

    [2-15] 암호화된 비밀번호 비교 로직 추가

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -73,6 +73,10 @@ class User extends BaseEntity {
   @Column({ type: 'double precision', default: 0 })
   lastOrientation: number;
 
+  public comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+
   @BeforeInsert()
   @BeforeUpdate()
   async savePassword(): Promise<void> {

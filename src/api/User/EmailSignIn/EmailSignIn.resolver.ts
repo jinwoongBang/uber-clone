79ec97e9574b24aefc820e1a657f18890e3f9789
jinwoong-bang <jinwoong-bang@jinwoong-bangui-MacBookPro.local>
2020-08-10 20:42:48 +0900

    [2-31] 이메일 로그인을 위한 로직 작성중

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolver.ts
@@ -0,0 +1,31 @@
+import { Resolvers } from '../../../types/resolvers';
+import { EmailSignInMutationArgs, EmailSignInResponse } from 'src/types/graph';
+import User from '../../../entities/User';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (_, args: EmailSignInMutationArgs): Promise<EmailSignInResponse> => {
+      const { email } = args;
+      try {
+        const user = User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: 'No User with that email',
+            token: null,
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+
+
+    },
+  },
+};
+
+export default resolvers;
